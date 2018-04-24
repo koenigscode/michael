3d12f1096c317588d9e32328a7527933069a4530
michaelkoenig2002 <michael.koenig2002@yahoo.com>
2018-04-24 21:16:07 +0200

    fixed ie flexbox bug and finished main layout

--- gulpfile.js
+++ gulpfile.js
@@ -7,16 +7,16 @@ nested = require("postcss-nested"),
 cssImport = require("postcss-import");
 
 gulp.task("style", function() {
-  return gulp
-  .src("./src/style.css")
-  .pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
-  .pipe(gulp.dest("./public/css"));
+	return gulp
+	.src("./src/style.css")
+	.pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
+	.pipe(gulp.dest("./public/css"));
 });
 
 gulp.task("watch", function() {
-  gulp.start("style");
+	gulp.start("style");
 
-  watch("./src/**/*.css", function() {
-    gulp.start("style");
-  });
+	watch("./src/**/*.css", function() {
+		gulp.start("style");
+	});
 });
--- public/index.php
+++ public/index.php
@@ -4,75 +4,81 @@ require "./includes/head.php";
 ?>
 
 <body>
-	<header class="header section section--full flex-center">
-		<div class="header__container">
-			<h1 class="title">Michael.</h1>
-			<!-- <div class="header__subtitle-container">
-				<a href="#web" class="header__subtitle">Web-Entwicklung</a>
-				<a href="#foto" class="header__subtitle">Fotografie</a>
-				<a href="#prog" class="header__subtitle">Programmieren</a>
-			</div> -->
+	<header class="background background--header ie-center-fix">
+		<div class="header section section--full">
+			<div class="section__container">
+				<div class="header__container">
+					<h1 class="title">Michael.</h1>
+				</div>
+			</div>
+			<div class="arrow arrow--header"></div>
 		</div>
-		<div class="arrow arrow--header"></div>
 	</header>
 
-	<section class="web section flex-center" id="web">
-		<div class="web__container">
-			<h2 class="title title--secondary">Web-Entwicklung</h2>
-			<div class="info-container">
-				<p class="info">Ich habe mich über die letzten Monate mit den verschiedensten Web-Development Technologien auseinander gesetzt. Ich achte dabei stets auf moderne und responsive Websites, um Nutzern auf allen Endgeräten die best mögliche Erfahrung zu bieten.</p>
+	<section class="background background--header ie-center-fix">
+		<div class="web section" id="web">
+			<div class="section__container">
+				<h2 class="title title--secondary">Web-Entwicklung</h2>
+				<div class="info-container">
+					<p class="info">Ich habe mich über die letzten Monate mit den verschiedensten Web-Development Technologien auseinander gesetzt. Ich achte dabei stets auf moderne und responsive Websites, um Nutzern auf allen Endgeräten die best mögliche Erfahrung zu bieten.</p>
+				</div>
+				<div class="icon-container">
+					<span class="icon icon-html5"></span>
+					<span class="icon icon-css3"></span>
+					<span class="icon icon-sass"></span>
+					<span class="icon icon-javascript"></span>
+					<span class="icon icon-php"></span>
+					<span class="icon icon-node"></span>
+					<span class="icon icon-git"></span>
+					<span class="icon icon-github"></span>
+					<span class="icon icon-npm"></span>
+					<span class="icon icon-gulp"></span>
+				</div>
 			</div>
-			<div class="icon-container">
-				<span class="icon icon-html5"></span>
-				<span class="icon icon-css3"></span>
-				<span class="icon icon-sass"></span>
-				<span class="icon icon-javascript"></span>
-				<span class="icon icon-php"></span>
-				<span class="icon icon-node"></span>
-				<span class="icon icon-git"></span>
-				<span class="icon icon-github"></span>
-				<span class="icon icon-npm"></span>
-				<span class="icon icon-gulp"></span>
-			</div>
+			<div class="arrow arrow--web"></div>
 		</div>
-		<div class="arrow arrow--web"></div>
 	</section>
 
-	<section class="foto section flex-center" id="foto">
-		<div class="info__container">
-			<h2 class="title title--secondary">Fotografie</h2>
-			<div class="info-container">
-				<p class="info">In meiner Freizeit beschäftige ich mich auch gerne mit Fotografie und Bildbearbeitung. Ich nutze dazu die Canon EOS 700D sowie Adobe Photoshop und Lightroom.</p>
+	<section class="background background--header ie-center-fix">
+		<div class="foto section" id="foto">
+			<div class="section__container">
+				<h2 class="title title--secondary">Fotografie</h2>
+				<div class="info-container">
+					<p class="info">In meiner Freizeit beschäftige ich mich auch gerne mit Fotografie und Bildbearbeitung. Ich nutze dazu die Canon EOS 700D sowie Adobe Photoshop und Lightroom.</p>
+				</div>
+				<div class="icon-container">
+					<span class="icon icon-adobephotoshop"></span>
+					<span class="icon icon-adobelightroom"></span>
+				</div>
 			</div>
-			<div class="icon-container">
-				<span class="icon icon-adobephotoshop"></span>
-				<span class="icon icon-adobelightroom"></span>
-			</div>
+			<div class="arrow arrow--foto"></div>
 		</div>
-		<div class="arrow arrow--foto"></div>
 	</section>
 
-	<section class="prog section flex-center" id="prog">
-		<div class="info__container">
-			<h2 class="title title--secondary">Programmieren</h2>
-			<div class="info-container">
-				<p class="info">Nachdem ich mich über ein Jahr lang mit der Programmierung in C beschäftigt habe, lerne ich derzeit Java, eine objektorientierte und plattformunabhängige Programmiersprache, die überall (u.a. Android-Smartphones) vertreten ist.</p>
+	<section class="background background--header ie-center-fix">
+		<div class="prog section" id="prog">
+			<div class="section__container">
+				<h2 class="title title--secondary">Programmieren</h2>
+				<div class="info-container">
+					<p class="info">Nachdem ich mich über ein Jahr lang mit der Programmierung in C beschäftigt habe, lerne ich derzeit Java, eine objektorientierte und plattformunabhängige Programmiersprache, die überall (u.a. Android-Smartphones) vertreten ist.</p>
+				</div>
+				<div class="icon-container">
+					<span class="icon icon-c"></span>
+					<span class="icon icon-java"></span>
+				</div>
 			</div>
-			<div class="icon-container">
-				<span class="icon icon-c"></span>
-				<span class="icon icon-java"></span>
-			</div>
+			<div class="arrow arrow--prog"></div>
 		</div>
-		<div class="arrow arrow--prog"></div>
 	</section>
 
-	<section class="imp section section--small flex-center">
-		<div class="info__container">
-			<h2 class="title title--secondary">Impressum</h2>
-			<div class="info-container">
-				<p class="info">datei hier verlinken</p>
+	<footer class="background background--header ie-center-fix">
+		<div class="imp section section--small">
+			<div class="section__container">
+				<div class="info-container">
+					<a class="info info--link" href="./impressum.php">Impressum</a>
+				</div>
 			</div>
 		</div>
-	</section>
+	</footer>
 
 </body>
